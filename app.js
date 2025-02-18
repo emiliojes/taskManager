47e7d1ae132669587273d9dddd7f2da619898b66
Emilio Núñez <emiliojes@gmail.com>
2025-02-18 13:21:10 -0500

    Adding the creating task element

--- app.js
+++ app.js
@@ -8,5 +8,16 @@ taskForm.addEventListener("submit", (event) => {
   const taskInput = document.getElementById("task-input");
   const task = taskInput.value;
   console.log(task)
+  if(task) {
+    taskList.append(createTaskElement(task))
+    taskInput.value = '';
+  }
+
+  function createTaskElement(task){
+    const li = document.createElement("li")
+    li.textContent = task
+
+    
+  }
   
 });
