7d874a3bcdbd0768a574ea69b59905676293b0e3
Emilio Núñez <emiliojes@gmail.com>
2025-02-18 14:55:27 -0500

    Fixing error when you delete appear undefined, adding feature to asking you before to delete something

--- app.js
+++ app.js
@@ -9,15 +9,49 @@ taskForm.addEventListener("submit", (event) => {
   const task = taskInput.value;
   console.log(task)
   if(task) {
-    taskList.append(createTaskElement(task))
+    taskList.appendChild(createTaskElement(task))
     taskInput.value = '';
   }
 
   function createTaskElement(task){
     const li = document.createElement("li")
-    li.textContent = task
+    li.textContent = task;
 
+    const deleteButton = createButton("❌", "delete-btn");
+
+    //deleteButton.addEventListener("click", () => li.remove())
+
+    li.appendChild(deleteButton)
+    return li
     
   }
+
+  function createButton (text, className){
+    const btn = document.createElement("span")
+    btn.textContent = text;
+    btn.className = className;
+    return btn;
+  }
+
+   
+  taskList.addEventListener("click", (event) => {
+    //By clicking the element we will see class
+    //console.log(event.target);
+    if (event.target.classList.contains("delete-btn")){
+      deleteTask(event.target.parentElement)
+    }
+  })
+ 
+
+  function deleteTask (taskItem) {
+    if(confirm("Are you sure, you want to delete this item?")){
+      taskItem.remove()
+    }
+  }
+
+
+
+  
+
   
 });
